b096e6d0db700bfc6447eec724be42a3b9c7a08f
ashu-toshgoyal <ashu.toshgoyal1405@gmail.com>
2025-12-28 18:26:57 +0530

    JS LEft

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 let t = document.getElementById("time");
-const contenetspace = document.getElementById("contentid");
-const xp = document.getElementById("xp");
-const about = document.getElementById("about");
-const ashu_homepage = document.getElementById("home");
-const homepage = document.getElementById("contentid-1");
-const aboutpage = document.getElementById("contentid-2");
-const expage = document.getElementById("contentid-3");
 
 // 👇 FIXED — About works as Home (no element with id="home")
 // const home = document.getElementById("about");
@@ -22,13 +15,3 @@ function updateTime() {
 // run when page finishes loading
 setInterval(updateTime, 1000);
 
-ashu_homepage.addEventListener("click",()=>{
-        about.innerText = "About"
-        homepage.style.display ='block';
-        aboutpage.style.display='none'; 
-})
-about.addEventListener("click",()=>{
-        about.innerText = ">About"
-        homepage.style.display ='none';
-        aboutpage.style.display='block'; 
-})
